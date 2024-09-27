eb004acbfbae02fb2a268767b4e7bf577ac7a0f8
private-tsugawa <131883442+private-tsugawa@users.noreply.github.com>
2024-09-27 15:01:19 +0900

    add case4-3

--- general/decompressImage.sh
+++ general/decompressImage.sh
@@ -1,6 +1,6 @@
-for layer in $(cat image-file-pass/manifest.json | jq -c '.[0].Layers[]' | sed s/\"//g); 
+for layer in $(cat <path>/manifest.json | jq -c '.[0].Layers[]' | sed s/\"//g); 
 do
-    layer_dir=image-file-pass/blobs/sha256/layer-$(eval echo ${layer} | awk '{sub("blobs/sha256/", "");print $0;}') ;
+    layer_dir=<path>/blobs/sha256/layer-$(eval echo ${layer} | awk '{sub("blobs/sha256/", "");print $0;}') ;
     mkdir ${layer_dir} ;
-    tar xvf image-file-pass/${layer} -C ${layer_dir}  ; 
+    tar xvf <path>/${layer} -C ${layer_dir}  ; 
 done
